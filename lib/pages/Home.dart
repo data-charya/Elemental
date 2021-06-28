5cef970daf2cca4a690006b15d316168aadb25ee
Shanwill Pinto <shanwillpinto@gmail.com>
2021-06-28 13:35:27 +0530

    front page changes made

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -2,30 +2,60 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-import 'package:random_color/random_color.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-List f = [];
 var facts;
-List fact = [];
+int aqi;
+var aqiperc;
+var _color;
+var border;
+var co, h, no2, so2;
 
 class _HomePageState extends State<HomePage> {
-  RandomColor _randomColor = RandomColor();
   getdata() async {
-    String url = 'https://elementalapi.herokuapp.com/api/v2/';
+    String url =
+        'https://api.waqi.info/feed/india/?token=e174d708b34e40a42ab4c5b195eec0e370810db5';
     var data = await http.get(Uri.parse(url));
 
     facts = json.decode(data.body);
-    if (f.length < 20) {
-      for (var i = 0; i < 20; i++) {
-        f.add(facts['Element_data'][i]['info']);
-      }
+    setState(() {
+      aqi = facts['data']['aqi'];
+      aqiperc = aqi / 302;
+      co = facts['data']['iaqi']['co']['v'];
+      no2 = facts['data']['iaqi']['no2']['v'];
+      so2 = facts['data']['iaqi']['so2']['v'];
+      h = facts['data']['iaqi']['h']['v'].toStringAsPrecision(3);
+    });
+    if (aqi > 0 && aqi <= 50) {
       setState(() {
-        fact = f.reversed.toList();
+        _color = Color.fromRGBO(39, 174, 96, 1); //green
+        border = Color.fromRGBO(51, 214, 120, 1);
+      });
+    } else if (aqi > 50 && aqi <= 100) {
+      setState(() {
+        _color = Color.fromRGBO(241, 196, 15, 1); //yellow
+        border = Color.fromRGBO(240, 174, 7, 1);
+      });
+    } else if (aqi > 100 && aqi <= 150) {
+      setState(() {
+        _color = Color.fromRGBO(230, 126, 34, 1); //orange
+      });
+    } else if (aqi > 150 && aqi <= 200) {
+      setState(() {
+        _color = Color.fromRGBO(229, 57, 53, 1); //red
+      });
+    } else if (aqi > 200 && aqi <= 300) {
+      setState(() {
+        _color = Color.fromRGBO(187, 143, 206, 1); //purple
+      });
+    } else {
+      setState(() {
+        _color = Color.fromRGBO(144, 12, 63, 1); //maroon
       });
     }
   }
@@ -50,56 +80,293 @@ class _HomePageState extends State<HomePage> {
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 child: Container(
                   width: MediaQuery.of(context).size.width / 1.5,
-                  height: 50,
-                  child: Text(
-                    'Feeling curious ?',
-                    style: GoogleFonts.nunito(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white70,
-                    ),
+                  height: 70,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Feeling curious ?',
+                        style: GoogleFonts.nunito(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white70,
+                        ),
+                      ),
+                      Text(
+                        'Check out todays chemical presence in the air',
+                        style: GoogleFonts.nunito(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white54,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
             ],
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 1.3,
-            child: Container(
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: f.length,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "AQI ",
+                          textAlign: TextAlign.end,
+                          style: GoogleFonts.nunito(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          ),
                         ),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(
-                              f[index],
-                              textAlign: TextAlign.center,
+                        SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: LiquidCircularProgressIndicator(
+                            value: aqiperc, // Defaults to 0.5.
+                            valueColor: AlwaysStoppedAnimation(
+                                _color), // Defaults to the current Theme's accentColor.
+                            backgroundColor: Colors
+                                .white, // Defaults to the current Theme's backgroundColor.
+                            borderColor: border,
+                            borderWidth: 5.0,
+                            direction: Axis
+                                .vertical, // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+                            center: Text(
+                              aqi.toString(),
                               style: GoogleFonts.nunito(
-                                fontSize: 15,
+                                fontSize: 20,
                                 fontWeight: FontWeight.w800,
-                                color: Colors.black87,
+                                color: Colors.black54,
                               ),
                             ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                  );
-                },
+                  ),
+                ),
               ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 160,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "${co}" + ' m3',
+                                textAlign: TextAlign.end,
+                                style: GoogleFonts.nunito(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'CO',
+                                style: GoogleFonts.nunito(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 160,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "${h}" + ' m3',
+                                textAlign: TextAlign.end,
+                                style: GoogleFonts.nunito(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'H',
+                                style: GoogleFonts.nunito(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 160,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[300],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "${no2}" + ' m3',
+                                textAlign: TextAlign.end,
+                                style: GoogleFonts.nunito(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'NO2',
+                                style: GoogleFonts.nunito(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 160,
+                  height: 150,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.tealAccent[100],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "${so2}" + ' m3',
+                                textAlign: TextAlign.end,
+                                style: GoogleFonts.nunito(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'SO2',
+                                style: GoogleFonts.nunito(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
